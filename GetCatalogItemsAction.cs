01db8fd1eb0b81e8b88d54cbf98617a6adc122b8
Milosz Moczkowski <milosz.moczkowski@gmail.com>
2016-06-16 13:05:31 +0200

    Add GetCatalogItemsAction

---
+++ GetCatalogItemsAction.cs
@@ -0,0 +1,66 @@
+﻿/* MIT License
+ *
+ * Copyright (c) 2016 Sathra Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using HutongGames.PlayMaker;
+using PlayFab;
+using PlayFab.ClientModels;
+
+namespace Sathra.PlayMaker.PlayFab {
+
+[ActionCategory("PlayFab")]
+public class GetCatalogItemsAction : FsmStateAction {
+
+	public FsmString catalogVersion;
+
+	public FsmEvent successEvent;
+	public FsmEvent failureEvent;
+
+	[UIHint(UIHint.Variable)]
+	[ArrayEditor(VariableType.Unknown)]
+	public FsmArray items;
+
+	[UIHint(UIHint.Variable)]
+	public FsmString errorMessage;
+
+	public override void OnEnter() {
+		var request = new GetCatalogItemsRequest() {
+			CatalogVersion = catalogVersion.Value
+		};
+
+		PlayFabClientAPI.GetCatalogItems(request, OnSuccess, OnFailure);
+	}
+
+	private void OnSuccess(GetCatalogItemsResult result) {
+		items.Values = result.Catalog.ToArray();
+
+		Fsm.Event(successEvent);
+	}
+
+	private void OnFailure(PlayFabError error) {
+		errorMessage.Value = error.ErrorMessage;
+
+		Fsm.Event(failureEvent);
+	}
+}
+
+}
